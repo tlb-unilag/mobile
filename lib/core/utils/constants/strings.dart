66152c99d4771bb2a0f946e5615f5aff1a1da396
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-02-18 07:32:46 +0100

    feat:added app string as constants

---
+++ lib/core/utils/constants/strings.dart
@@ -0,0 +1,11 @@
+class AppStrings {
+  static const login = 'login';
+  static const signup = 'Sign Up';
+  static const getstarted = 'Get Started';
+  static const taroleafblight = 'Taro Leaf Blight Detector';
+  static const taroleafblightdesc =
+      'Detect taro leaf blight in your plants easily with our app';
+  static const home = 'home';
+  static const capture = 'capture';
+  static const profile = 'profile';
+}
