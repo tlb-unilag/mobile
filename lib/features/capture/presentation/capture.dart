6f92cf6a04e16337154f280726781a6465613c61
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-02-18 07:38:24 +0100

    feat:added capture page

---
+++ lib/features/capture/presentation/capture.dart
@@ -0,0 +1,15 @@
+import 'package:taro_leaf_blight/packages/packages.dart';
+
+class CaptureScreen extends ConsumerStatefulWidget {
+  const CaptureScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CaptureScreenState();
+}
+
+class _CaptureScreenState extends ConsumerState<CaptureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
