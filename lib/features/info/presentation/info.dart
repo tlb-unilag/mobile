fba0f62742391e654d15c853ee25f643a39d03dd
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-02-23 05:24:14 +0100

    feat:added onboarding and info screen

---
+++ lib/features/info/presentation/info.dart
@@ -0,0 +1,10 @@
+import 'package:taro_leaf_blight/packages/global_packages.dart';
+
+class DetectionInfoScreen extends ConsumerWidget {
+  const DetectionInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Placeholder();
+  }
+}
---
+++ lib/features/onboarding/presentation/onboarding.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taro_leaf_blight/core/utils/constants/strings.dart';
+import 'package:taro_leaf_blight/features/auth/presentation/login.dart';
+import 'package:taro_leaf_blight/packages/tabs/custom_tab_bar.dart';
+import 'package:taro_leaf_blight/widgets/custom_button.dart';
+
+import '../../../packages/packages.dart';
+
+class OnboardingScreen extends ConsumerStatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding:
+            EdgeInsets.only(left: 20.w, right: 20.w, top: 15.2.w, bottom: 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+              AppStrings.taroleafblight,
+              style: CustomTextStyle.label2XLBold.copyWith(letterSpacing: 0.8),
+            ),
+            Text(
+              AppStrings.taroleafblightdesc,
+              style:
+                  CustomTextStyle.paragraphMedium.copyWith(letterSpacing: 0.05),
+            ),
+              ],
+            )
+          ,
+             const Center(
+              child: CircleAvatar(
+              radius: 150,
+              backgroundImage:AssetImage(
+                 'assets/images/taro_leaf_blight.png'
+                 ,
+              )
+             )
+            ,
+             ),
+            38.gap,
+            Center(
+              child: AppButton(
+                backgroundColor: AppColors.primary,
+                onPressed: () {
+                  pushTo(const LoginScreen());
+                },
+                label: "Get Started",
+                // width: 100.w,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
