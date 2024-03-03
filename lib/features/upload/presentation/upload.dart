e608e011956ef3f126e8979f2148d679351962ec
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-03-03 13:24:54 +0100

    feat: added image and cropping logic to upload page

--- lib/features/upload/presentation/upload.dart
+++ lib/features/upload/presentation/upload.dart
@@ -1,6 +1,8 @@
 import 'package:taro_leaf_blight/features/capture/service/image_picker_service.dart';
+import 'package:taro_leaf_blight/features/detection/provider/detection_provider.dart';
 import 'package:taro_leaf_blight/features/upload/providers/cloudinary_url_generator_provider.dart';
 import 'package:taro_leaf_blight/features/upload/service/cloudinary_api.dart';
+import 'package:taro_leaf_blight/main.dart';
 import 'package:taro_leaf_blight/packages/packages.dart';
 
 class UploadScreen extends ConsumerStatefulWidget {
@@ -23,6 +25,7 @@ class _UploadScreenState extends ConsumerState<UploadScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16).copyWith(top: 0),
         child: ListView(
+          shrinkWrap: true,
           children: [
             Container(
               height: context.screenHeight,
@@ -35,14 +38,21 @@ class _UploadScreenState extends ConsumerState<UploadScreen> {
                         AppButton(
                           onPressed: () async {
                             try {
-                              file = await localImagePickerService
-                                  .getImage(ImageSource.camera);
+                              // i get a fileNotfOUND EXCEPTION because of the way i wrote the cropped logic , i gave it '' empty string
+                              var croppedImage = await localImagePickerService
+                                  .getImage(ImageSource.camera)
+                                  .then((value) =>
+                                      localImagePickerService.cropImage(value));
+                              setState(() {
+                                file = croppedImage;
+                              });
                             } catch (e) {
                               setState(() {
                                 _pickImageError = e;
                                 print(_pickImageError);
                               });
                             }
+                             pop(context);
                           },
                           label: "Take a picture",
                         ),
@@ -50,125 +60,184 @@ class _UploadScreenState extends ConsumerState<UploadScreen> {
                         AppButton(
                           onPressed: () async {
                             try {
-                              file = await localImagePickerService
-                                  .getImage(ImageSource.gallery);
-
-                              setState(() {});
+                              var croppedImage = await localImagePickerService
+                                  .getImage(ImageSource.gallery)
+                                  .then((value) =>
+                                      localImagePickerService.cropImage(value));
+                              setState(() {
+                                file = croppedImage;
+                              });
                             } catch (e) {
                               setState(() {
                                 _pickImageError = e;
                                 print(_pickImageError);
                               });
                             }
+                             pop(context);
                           },
                           label: "Select from gallery",
                           child: const Text("Select from gallery"),
                         ),
                       ]);
                     },
-                    child: Container(
-                        width: context.screenWidth - 50,
-                        height: context.screenHeight - 600,
-                        color: AppColors.greyDisabled,
-                        child:  Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.add_a_photo_outlined,
-                              size: 60,
+                    // modify the image in such a way that it uses extendedImage and taps to view full screen but the image height is fixed
+                    //
+                    child: file != null
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                const Text(
+                                  "Image Preview",
+                                  style: CustomTextStyle.paragraphMedium,
+                                ),
+                                Image.file(file!)
+                              ])
+                        : Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: AppColors.greyDisabled,
                             ),
-                            Text(
-                              "Upload a photo",
-                              style: CustomTextStyle.textsmall14.withColorHex(0xFF17171B),
-                            )
-                          ],
-                        )),
+                            width: context.screenWidth - 50,
+                            height: context.screenHeight - 600,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                if (file == null)
+                                  const Icon(
+                                    Icons.add_a_photo_outlined,
+                                    size: 50,
+                                    weight: 20.0,
+                                  ),
+                                if (file == null)
+                                  Text(
+                                    "Upload a photo",
+                                    style: CustomTextStyle.textsmall14
+                                        .withColorHex(0xFF17171B),
+                                  )
+                              ],
+                            )),
                   ),
                   10.gap,
-                  // if (file != null) ...[
-                  //    AppButton(
-                  //     onPressed: () {
-                  //       // Detect image fn execution
-                  //     },
-                  //     label: "Detect image",
-                  //   ),
-                  // ],
-                  AppButton(
-                    onPressed: () async {
-                      Dialogs.openBottomSheet(children: [
-                        AppButton(
+                  file != null
+                      ? AppButton(
                           onPressed: () async {
-                            try {
-                              var fileFromCamera = await localImagePickerService
-                                  .getImage(ImageSource.camera);
-                              setState(() {
-                                file = fileFromCamera;
-                              });
-                            } catch (e) {
-                              setState(() {
-                                _pickImageError = e;
-                                print(_pickImageError);
-                              });
-                            }
+                            Dialogs.openBottomSheet(children: [
+                              AppButton(
+                                onPressed: () async {
+                                  try {
+                                    var croppedImage =
+                                        await localImagePickerService
+                                            .getImage(ImageSource.camera)
+                                            .then((value) =>
+                                                localImagePickerService
+                                                    .cropImage(value));
+                                    setState(() {
+                                      file = croppedImage;
+                                    });
+                                  } catch (e) {
+                                    setState(() {
+                                      _pickImageError = e;
+                                      print(_pickImageError);
+                                    });
+                                  }
+                                },
+                                label: "Take a picture",
+                              ),
+                              10.gap,
+                              AppButton(
+                                onPressed: () async {
+                                  try {
+                                    var croppedImage =
+                                        await localImagePickerService
+                                            .getImage(ImageSource.gallery)
+                                            .then((value) =>
+                                                localImagePickerService
+                                                    .cropImage(value));
+                                    setState(() {
+                                      file = croppedImage;
+                                    });
+                                  } catch (e) {
+                                    setState(() {
+                                      _pickImageError = e;
+                                      print(_pickImageError);
+                                    });
+                                  }
+                                  pop(context);
+                                },
+                                label: "Select from gallery",
+                              ),
+                            ]);
                           },
-                          label: "Take a picture",
-                        ),
-                        10.gap,
-                        AppButton(
+                          label: "Select another image",
+                        )
+                      : AppButton(
                           onPressed: () async {
-                            try {
-                              var fileFromGallery =
-                                  await localImagePickerService
-                                      .getImage(ImageSource.gallery);
-                              setState(() {
-                                file = fileFromGallery;
-                              });
-                            } catch (e) {
-                              setState(() {
-                                _pickImageError = e;
-                                print(_pickImageError);
-                              });
-                            }
-                            pop(context);
+                            Dialogs.openBottomSheet(children: [
+                              AppButton(
+                                onPressed: () async {
+                                  try {
+                                    var croppedImage =
+                                        await localImagePickerService
+                                            .getImage(ImageSource.camera)
+                                            .then((value) =>
+                                                localImagePickerService
+                                                    .cropImage(value));
+                                    setState(() {
+                                      file = croppedImage;
+                                    });
+                                  } catch (e) {
+                                    setState(() {
+                                      _pickImageError = e;
+                                      print(_pickImageError);
+                                    });
+                                  }
+                                   pop(context);
+                                },
+                                label: "Take a picture",
+                              ),
+                              10.gap,
+                              AppButton(
+                                onPressed: () async {
+                                  try {
+                                    var croppedImage =
+                                        await localImagePickerService
+                                            .getImage(ImageSource.gallery)
+                                            .then((value) =>
+                                                localImagePickerService
+                                                    .cropImage(value));
+                                    setState(() {
+                                      file = croppedImage;
+                                    });
+                                  } catch (e) {
+                                    setState(() {
+                                      _pickImageError = e;
+                                      print(_pickImageError);
+                                    });
+                                  }
+                                  pop(context);
+                                },
+                                label: "Select from gallery",
+                              ),
+                            ]);
                           },
-                          label: "Select from gallery",
+                          label: "Choose an image to detect",
                         ),
-                      ]);
-                    },
-                    label: "Select another Image",
-                  ),
-
                   10.gap,
-                  // this is just here for checking the url sake
                   if (file != null) ...[
                     AppButton(
-                        label: "Generate Url",
+                        label: "Detect Image",
                         onPressed: () async {
-                          // ref.watch(generateUrlProvider(file!));
-                          print('this is file:${file}');
                           var res = await cloudinaryService
-                              .uploadImage(file!)
-                              .then((value) => print(value.data?.url));
-                          // setState(() {
-                          //   urlLink = url;
-                          // });
+                              .uploadImage(file!, context)
+                              .then((value) {
+                            print(value);
+                            // detectOneImage(value.data!.url)
+                            detectOneImage(context);
+                          });
                         }),
-                    // if (urlLink != null)
-                    //   urlLink!.when(
-                    //       skipLoadingOnRefresh: false,
-                    //       data: (data) {
-                    //         print(data);
-                    //         return Text(data.data);
-                    //       },
-                    //       loading: () {
-                    //         return const CircularProgressIndicator();
-                    //       },
-                    //       error: (error, stackTrace) {
-                    //         print(error);
-                    //         return Text(error.toString());
-                    //       }),
-                  ]
-                  ////
+                  ],
+                  10.gap,
+                  // this is just here for checking the url sake
                 ],
               ),
             )
@@ -179,9 +248,8 @@ class _UploadScreenState extends ConsumerState<UploadScreen> {
   }
 }
 
-// image is displayed here
-
-//loading spinner doesn't want to stop loading , we get a 401, an eror ought to be thrown and handled by the error block
 // upload image -> a loading spinner is shown with the text -> uploading image (lets add a progress bar)
 
-// select another image
+//refactor the bottomSheet logic 
+
+// fix the cropping logic , null conditional logic 
