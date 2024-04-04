1fba721e117e61b0bc40c0a1f2e4b7c75072a06e
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-04-04 16:41:14 +0100

    feat:added app crash error view

---
+++ lib/widgets/app_crash_error_view.dart
@@ -0,0 +1,65 @@
+// import 'package:flutter/material.dart';
+// import 'package:taro_leaf_blight/core/utils/constants/strings.dart';
+
+
+// class AppCrashErrorView extends StatelessWidget {
+//   const AppCrashErrorView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final ThemeData theme = context.theme;
+//     final TextTheme textTheme = theme.textTheme;
+//     final L10n l10n = context.l10n;
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: SingleChildScrollView(
+//         padding: const EdgeInsets.symmetric(horizontal: 24),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             const SizedBox(height: 40),
+//             // const Align(alignment: Alignment.centerLeft, child: AppIcon()),
+//             const Align(alignment: Alignment.centerLeft, child: Icon(Icons.error)),
+//             const SizedBox(height: 38),
+//             Text(
+//               AppStrings.crashViewTitleMessage,
+//               style: textTheme.displaySmall!.copyWith(
+//                 height: 1.08,
+//                 fontWeight: ,
+//                 color: theme.colorScheme.onBackground,
+//               ),
+//             ),
+//             const SizedBox(height: 35),
+//             Text.rich(
+//               TextSpan(
+//                 children: <TextSpan>[
+//                   TextSpan(
+//                     text: AppStrings.crashViewQuoteMessage,
+//                     style: textTheme.headlineSmall!.copyWith(height: 1.5),
+//                   ),
+//                   TextSpan(text: '  —  ', style: textTheme.titleMedium),
+//                   TextSpan(
+//                     text: AppStrings.crashViewQuoteAuthor,
+//                     style: textTheme.titleMedium!
+//                         .copyWith(fontStyle: FontStyle.italic),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             const SizedBox(height: 24),
+//             Text(
+//               AppStrings.crashViewBugMessage1,
+//               style: textTheme.titleMedium!.copyWith(height: 1.45),
+//             ),
+//             const SizedBox(height: 24),
+//             Text(
+//               AppStrings.crashViewBugMessage2,
+//               style: textTheme.titleMedium!.copyWith(height: 1.45),
+//             ),
+//             const SizedBox(height: 24),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
