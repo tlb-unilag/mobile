4e52260717c0423cbdba157ce3a864ac220d9637
ibkisthere <ibukunoluwaakintobi@gmail.com>
2024-05-31 02:19:35 +0100

    feat:added solutions section to widget

--- lib/features/info/presentation/info.dart
+++ lib/features/info/presentation/info.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:taro_leaf_blight/core/utils/extensions/date_extensions.dart';
-import 'package:taro_leaf_blight/features/detection/models/detection_model.dart';
 import 'package:taro_leaf_blight/features/detection/provider/detection_providers.dart';
 import 'package:taro_leaf_blight/features/error/presentation/error.dart';
-import 'package:taro_leaf_blight/packages/global_packages.dart';
 import 'package:taro_leaf_blight/packages/packages.dart';
 import 'package:taro_leaf_blight/widgets/custominfo_widget.dart';
 import 'package:taro_leaf_blight/widgets/image_viewer.dart';
@@ -32,62 +29,62 @@ class DetectionInfoScreen extends ConsumerWidget {
                 padding: const EdgeInsets.all(16.0),
                 child: detection.when(
                   data: (data) {
-                    return ListView(
-                      shrinkWrap: true,
+                    return Column(
                       children: [
-                        GestureDetector(
-                          onTap: () => showImageViewer(
-                            context,
-                            data.data!.imageUrl,
-                             swipeDismissible: true,
-                              doubleTapZoomable: true,
-                              // when immersive == true it gets janky swithcing from SystemOverlay modes 
-                              immersive: false
-                          ),
-                          child: Container(
-                              height: 290.h,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
-                                child: ExtendedImage.network(
-                                  data.data!.imageUrl,
-                                  fit: BoxFit.cover,
-                                  mode: ExtendedImageMode.gesture,
-                                ),
-                              )),
-                        ),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(8.0)
-                                    .copyWith(top: 10.0, bottom: 16.0),
-                                child: const Text("Count",
-                                    style: CustomTextStyle.label2XLXBold),
+                            GestureDetector(
+                        onTap: () => showImageViewer(
+                            context, data.data!.imageUrl,
+                            swipeDismissible: true,
+                            doubleTapZoomable: true,
+                            // when immersive == true it gets janky switching from SystemOverlay modes
+                            immersive: false),
+                        child: Container(
+                            height: 290.h,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: ExtendedImage.network(
+                                data.data!.imageUrl,
+                                fit: BoxFit.cover,
+                                mode: ExtendedImageMode.gesture,
                               ),
-                              CustomInfoWidget(
-                                color: Colors.grey.shade100,
-                                labelText: "Taro Early",
-                                data: data.data!.taroEarly,
-                              ),
-                              CustomInfoWidget(
-                                color: Colors.white,
-                                labelText: "Taro Not Early",
-                                data: data.data!.taroNotEarly,
-                              ),
-                              CustomInfoWidget(
-                                color: Colors.grey.shade100,
-                                labelText: "Taro Healthy",
-                                data: data.data!.taroHealthy,
-                              ),
-                              CustomInfoWidget(
-                                color: Colors.white,
-                                labelText: "Date",
-                                data: DateTime.parse(data.data!.createdAt)
-                                    .formatDateAndTime,
-                              ),
-                            ]),
-                      ],
-                    );
+                            )),
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0)
+                                  .copyWith(top: 10.0, bottom: 16.0),
+                              child: const Text("Count",
+                                  style: CustomTextStyle.label2XLXBold),
+                            ),
+                            CustomInfoWidget(
+                              color: Colors.grey.shade100,
+                              labelText: "Early",
+                              data: data.data!.taroEarly,
+                            ),
+                            CustomInfoWidget(
+                              color: Colors.white,
+                              labelText: "Not Early",
+                              data: data.data!.taroNotEarly,
+                            ),
+                            CustomInfoWidget(
+                              color: Colors.grey.shade100,
+                              labelText: "Healthy",
+                              data: data.data!.taroHealthy,
+                            ),
+                            CustomInfoWidget(
+                              color: Colors.white,
+                              labelText: "Date",
+                              data: DateTime.parse(data.data!.createdAt)
+                                  .formatDateAndTime,
+                            ),
+                          ]),
+                      SolutionWidget(
+                          solutionText: data.data!.solution,
+                          color: Colors.grey.shade100)
+                        ]
+                      );
                   },
                   loading: () {
                     return Skeletonizer(
@@ -114,17 +111,17 @@ class DetectionInfoScreen extends ConsumerWidget {
                                     ),
                                     CustomInfoWidget(
                                       color: Colors.grey.shade100,
-                                      labelText: "Taro Early",
+                                      labelText: "Early",
                                       data: 1,
                                     ),
                                     const CustomInfoWidget(
                                       color: Colors.white,
-                                      labelText: "Taro Not Early",
+                                      labelText: "Not Early",
                                       data: 2,
                                     ),
                                     CustomInfoWidget(
                                       color: Colors.grey.shade100,
-                                      labelText: "Taro Healthy",
+                                      labelText: "Healthy",
                                       data: 3,
                                     ),
                                     const CustomInfoWidget(
@@ -150,6 +147,36 @@ class DetectionInfoScreen extends ConsumerWidget {
                 )),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SolutionWidget extends StatelessWidget {
+  final String solutionText;
+  final Color color;
+  const SolutionWidget(
+      {required this.solutionText, super.key, required this.color});
+// fontSize: 18.sp
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: Text(
+                solutionText,
+                style: CustomTextStyle.paragraphMedium,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
